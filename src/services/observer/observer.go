25edfffc6ee86d7900271082279a6be455afc9ba
Victoria <vlopser@upv.es>
2024-01-23 16:45:05 +0100

    Adding new metric for the observer

--- src/services/observer/observer.go
+++ src/services/observer/observer.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gin-gonic/gin"
-	"github.com/nats-io/nats.go"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/nats-io/nats.go"
 )
 
-var count = 0
+var count int
 var lowerLimit = 200
 var upperLimit = 1000
 var workers = 1
 
-const (
-	REQUEST_QUEUE = "request_queue"
-)
-
+// ******* Accstatz Data structur *************
 type body struct {
 	ServerId     string         `json:"server_id"`
 	Now          time.Time      `json:"now"`
@@ -42,10 +40,19 @@ type accountStatz struct {
 	SlowConsumers    int          `json:"slow_consumers"`
 }
 
+//******* Accstatz Data structur *************
+
+// ******* Jsz Data structur *************
+type jsz struct {
+	Streams int `json:"streams"`
+}
+
+// ******* Jsz Data structur *************
+
 var running = false
 
 func scheduledThread() {
-	ticker1 := time.NewTicker(time.Second * 1)
+	ticker1 := time.NewTicker(time.Second * 5)
 	for {
 		select {
 		case <-ticker1.C:
@@ -73,40 +80,72 @@ func checkQueue() {
 	}
 	log.Println("Response is:\n", msg)
 
-	count -= msg.AccountStatz[0].Sent.Msgs + msg.AccountStatz[0].Received.Msgs
-	if workers > 1 && count < lowerLimit {
-		log.Printf("Received/sent messages in last 1s : %d", count)
+	totalMsgs := msg.AccountStatz[0].Sent.Msgs + msg.AccountStatz[0].Received.Msgs - count
+	decidedNumberOfWorkers(totalMsgs)
+}
+
+func decidedNumberOfWorkers(totalMsgs int) {
+	if workers > 1 && totalMsgs < lowerLimit {
+		log.Printf("Received/sent messages : %d", totalMsgs)
 		log.Printf("Limit : %d", lowerLimit)
 		workers--
 		log.Printf("Reducing number of workers, n° workers : %d", workers)
 		upperLimit /= 2 * workers
-	} else if count > upperLimit {
+	} else if totalMsgs > upperLimit {
 		log.Println("Nats queue has exceeded current limit of sent/received messages")
-		log.Printf("Received/sent messages in last 1s : %d", count)
+		log.Printf("Received/sent messages : %d", totalMsgs)
 		log.Printf("Limit : %d", upperLimit)
 		workers++
 		log.Printf("Adding new worker, n° workers : %d", workers)
 		upperLimit *= 2
 	}
+
 }
 
-func getMetrics(ctx *gin.Context) {
-	//endpoint := "http://localhost:8222/accstatz"
-	// todo put other metrics that you consider important
+func checkJsz() int {
+	endpoint := "http://localhost:8222/jsz"
+	response, err := http.Post(endpoint, "application/json", nil)
+	if err != nil {
+		log.Printf("Error connecting to endpoint %s", endpoint)
+	}
+	var msg jsz
+	bodyText, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("Error reading JSON:", err)
+		return -1
+	}
+	err = json.Unmarshal(bodyText, &msg)
+	if err != nil {
+		log.Println("Error when unmarshalling JSON:", err)
+		return -1
+	}
+	return msg.Streams
+}
 
+var numberOfWorksDoneBefore = 0
+
+const periodoTiempo = 5
+
+func averageRateOfService() {
+	//Hay que revisarlo, se debe restar uno pk no debemos tener en cuenta el stream KV__
+	numberOfWorksDoneAfter := checkJsz()
+	average := float64(numberOfWorksDoneAfter-numberOfWorksDoneBefore) / periodoTiempo
+	numberOfWorksDoneBefore = numberOfWorksDoneAfter
+	log.Println("La tasa promedio de servicio es: ", average)
+	time.Sleep(periodoTiempo * time.Second)
 }
 
 func main() {
-	router := gin.Default()
-
-	natsServer, err := nats.Connect(os.Getenv("NATS_SERVER_ADDRESS")) //nats.DefaultURL
+	nats_server, err := nats.Connect(os.Getenv("NATS_SERVER_ADDRESS")) //nats.DefaultURL
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer natsServer.Close()
+	defer nats_server.Close()
 
-	router.POST("/metrics", func(ctx *gin.Context) { getMetrics(ctx) })
-	router.Run(":8080")
+	//go scheduledThread()
+	for {
+		averageRateOfService()
+	}
 
-	go scheduledThread()
+	fmt.Println("El número de trabajos realizados es", checkJsz())
 }
